18c2d62bfe73dcf16daf423e3289ec55b20cc46b
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2024-09-08 03:05:10 +0530

    Create Token with Metadata

--- 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,71 +1,85 @@
 import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
-import {
-    createInitializeMint2Instruction,
-    getMinimumBalanceForRentExemptMint,
-    MINT_SIZE,
-    TOKEN_2022_PROGRAM_ID,
-} from "@solana/spl-token";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+    TOKEN_2022_PROGRAM_ID,
+    getMintLen,
+    createInitializeMetadataPointerInstruction,
+    createInitializeMintInstruction,
+    TYPE_SIZE,
+    LENGTH_SIZE,
+    ExtensionType,
+} from "@solana/spl-token";
+import { createInitializeInstruction, pack } from "@solana/spl-token-metadata";
 
 export function TokenLaunchpad() {
     const { connection } = useConnection();
     const wallet = useWallet();
-    // console.log({ wallet });
-    // console.log({MINT_SIZE})//82
 
-    const createToken = async () => {
-        console.log("Creating Token");
-        // const name = document.getElementById("name").value;
-        // const symbol = document.getElementById("symbol").value;
-        // const url = document.getElementById("url").value;
-        // const amount = document.getElementById("amount").value;
+    async function createToken() {
+        const mintKeypair = Keypair.generate();
+        console.log({ mintKeypair });
+        const metadata = {
+            mint: mintKeypair.publicKey,
+            name: "NISHA",
+            symbol: "ANT",
+            uri: "https://raw.githubusercontent.com/NishantCoder108/BeatQ/resources/metadata.json",
+            additionalMetadata: [],
+        };
 
-        // console.log(name, url, symbol, amount);
-        try {
-            const mintKeypair = Keypair.generate();
+        const mintLen = getMintLen([ExtensionType.MetadataPointer]);
+        const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(metadata).length;
 
-            const lamports = await getMinimumBalanceForRentExemptMint(
-                connection
-            );
+        const lamports = await connection.getMinimumBalanceForRentExemption(
+            mintLen + metadataLen
+        );
 
-            console.log(mintKeypair.publicKey);
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: mintKeypair.publicKey,
+                space: mintLen,
+                lamports,
+                programId: TOKEN_2022_PROGRAM_ID,
+            }),
+            createInitializeMetadataPointerInstruction(
+                mintKeypair.publicKey,
+                wallet.publicKey,
+                mintKeypair.publicKey,
+                TOKEN_2022_PROGRAM_ID
+            ),
+            createInitializeMintInstruction(
+                mintKeypair.publicKey,
+                9,
+                wallet.publicKey,
+                null,
+                TOKEN_2022_PROGRAM_ID
+            ),
+            createInitializeInstruction({
+                programId: TOKEN_2022_PROGRAM_ID,
+                mint: mintKeypair.publicKey,
+                metadata: mintKeypair.publicKey,
+                name: metadata.name,
+                symbol: metadata.symbol,
+                uri: metadata.uri,
+                mintAuthority: wallet.publicKey,
+                updateAuthority: wallet.publicKey,
+            })
+        );
 
-            const transaction = new Transaction().add(
-                SystemProgram.createAccount({
-                    fromPubkey: wallet.publicKey,
-                    newAccountPubkey: mintKeypair.publicKey,
-                    lamports,
-                    programId: TOKEN_2022_PROGRAM_ID,
-                    space: MINT_SIZE,
-                }),
-                createInitializeMint2Instruction(
-                    mintKeypair.publicKey,
-                    9,
-                    wallet.publicKey,
-                    wallet.publicKey,
-                    TOKEN_2022_PROGRAM_ID
-                )
-            );
+        transaction.feePayer = wallet.publicKey;
+        transaction.recentBlockhash = (
+            await connection.getLatestBlockhash()
+        ).blockhash;
+        transaction.partialSign(mintKeypair);
 
-            transaction.feePayer = wallet.publicKey;
-            transaction.recentBlockhash = (
-                await connection.getLatestBlockhash()
-            ).blockhash;
-            transaction.partialSign(mintKeypair);
+        const sentTransaction = await wallet.sendTransaction(
+            transaction,
+            connection
+        );
 
-            const sentTransaction = await wallet.sendTransaction(
-                transaction,
-                connection
-            );
-            console.log({ sentTransaction });
+        console.log({ sentTransaction });
+    }
 
-            console.log(
-                `Token mint created at ${mintKeypair.publicKey.toBase58()}`
-            );
-        } catch (error) {
-            console.log(error);
-        }
-    };
     return (
         <div
             style={{
@@ -81,19 +95,16 @@ export function TokenLaunchpad() {
                 className="inputText"
                 type="text"
                 placeholder="Name"
-                id="name"
             ></input>{" "}
             <br />
             <input
                 className="inputText"
                 type="text"
-                id="symbol"
                 placeholder="Symbol"
             ></input>{" "}
             <br />
             <input
                 className="inputText"
-                id="url"
                 type="text"
                 placeholder="Image URL"
             ></input>{" "}
@@ -101,7 +112,6 @@ export function TokenLaunchpad() {
             <input
                 className="inputText"
                 type="text"
-                id="amount"
                 placeholder="Initial Supply"
             ></input>{" "}
             <br />
